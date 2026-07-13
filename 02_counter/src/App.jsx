4491b720e18448918e517ff560588635488490ee
Nishu Kumari <nishujha.nj01@gmail.com>
2026-07-13 13:34:13 +0530

    02_counter

---
+++ 02_counter/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  let [counter,setCounter] = useState(15)
+
+  //usestate : updates are sent in batches 
+
+  //let counter =5
+
+  const addValue = () => {
+    // console.log("clicked",counter);
+    //  counter=counter+1;
+    setCounter((prevCounter)=> prevCounter+1);
+    setCounter((prevCounter)=> prevCounter+1);
+    setCounter((prevCounter)=> prevCounter+1);
+    setCounter((prevCounter)=> prevCounter+1);
+  }
+
+  const removeValue=()=>{
+    // console.log("clicked",counter);
+    if(counter>0){
+    setCounter(counter--);}
+  }
+
+  return (
+    <>
+     <h1>Chai aur react</h1>
+     <h2>Counter value : {counter}</h2>
+
+     <button
+     onClick={addValue}
+     >Add value {counter}</button>
+     <br/>
+     <button
+     onClick={removeValue}
+     >remove value {counter}</button>
+     <p>footer : {counter}</p>
+    </>
+  )
+}
+
+export default App
